bf4fcbec5af3cd699c37150fdee26644d5392400
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-06-13 11:58:31 +0100

    Format book instance due back date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,13 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual property: bookInstance's Due Back Date (formatted)
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export the model
