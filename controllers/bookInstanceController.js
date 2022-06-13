ce8dc1e015900ae4c0d15eb118cd91e3f9d36d0d
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-06-13 18:22:34 +0100

    Implement book copies detail page

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -12,8 +12,20 @@ exports.bookinstance_list = function(req, res, next) {
 };
 
 // Display detail page for a specific bookinstance
-exports.bookinstance_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function(err, bookinstance) {
+      if (err) { return next(err) }
+      if (bookinstance==null) {
+        // There are no results
+        const err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render('bookinstance_detail', { title: 'Copy: ' + bookinstance.book.title, bookinstance: bookinstance });
+    });
 };
 
 // Display bookinstance create form on GET
