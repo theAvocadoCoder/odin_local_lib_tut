a6d5f4b38e2c74463926944c692f04ba5dcec02a
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-06-13 16:18:50 +0100

    Implement genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
+
+const async = require('async');
 
 // Display list of all genres
 exports.genre_list = function(req, res, next) {
@@ -12,8 +15,27 @@ exports.genre_list = function(req, res, next) {
 };
 
 // Display detail page for a specific genre
-exports.genre_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function(req, res, next) {
+  async.parallel({
+    genre: function(callback) {
+      Genre.findById(req.params.id)
+        .exec(callback);
+    },
+    genre_books: function(callback) {
+      Book.find({ 'genre': req.params.id })
+        .exec(callback);
+    },
+  }, function(err, results) {
+    if (err) { return next(err) }
+    if (results.genre==null) {
+      // There are no results
+      var err = new Error('Genre not found');
+      err.status = 404;
+      return next(err);
+    }
+    // Successful, so render
+    res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books });
+  });
 };
 
 // Display genre create form on GET
